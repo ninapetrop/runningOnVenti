d75ee1068cf2802a52114a9ea5e922f03c23ec86
Nina Petropoulos <petropoulos.nina@gmail.com>
2016-06-10 15:25:44 -0400

    responsive styling fixes to "top" section

--- index.php
+++ index.php
@@ -15,30 +15,15 @@
 <section id='top'>
 	<!-- Show the three most recent posts -->
 	<div id='article-carousel'>
-		<?php
-		// Grab the first thre ...
-		$args = array( 'posts_per_page' => 1, 'offset'=> 0);
-		$myposts = get_posts( $args );
-		foreach ( $myposts as $post ) : setup_postdata( $post ); ?>
-
-			<div class='article-group active'>
-				<!-- Set the feature_image as the background image -->
-				<?php
-$src = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), array( 5600,1000 ), false, '' );
-?>
-				<div class='preview-image' style="background-image: url(<?php echo $src[0]; ?> ) !important;"></div>
-				<!-- Fill the content box -->
-				<div class='preview-excerpt'>
-					<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-					<p class='excerpt'><?php the_excerpt(); ?></p>
-				</div>
+		<!-- Javascript will fill in the info for these boxes -->
+		<div class='article-group active'>
+			<div class='article-image'>
+				<!-- Image for 'active' article in carousel goes here -->
 			</div>
-
-		<?php endforeach;
-		wp_reset_postdata();?>
+		</div>
 
 		<!-- What is being cycled through  -->
-		<div class='cycle'>
+		<div class='cycle-group'>
 			<?php
 			// Grab the first thre ...
 			$args = array( 'posts_per_page' => 3, 'offset'=> 0);
@@ -48,13 +33,12 @@ $src = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), array( 560
 				<div class='article-group'>
 
 					<!-- Set the feature_image as the background image -->
-					<?php
-	$src = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), array( 5600,1000 ), false, '' );
-	?>
-					<div class='preview-image' style="background-image: url(<?php echo $src[0]; ?> ) !important;"></div>
+					<?php $src = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), array( 5600,1000 ), false, '' ); ?>
+					<div class='article-image' style="background-image: url(<?php echo $src[0]; ?> ) !important;"></div>
 
 					<!-- Fill the content box -->
-					<div class='preview-excerpt'>
+					<div class='article-excerpt'>
+						<p class='meta'>The Date</p>
 						<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
 						<p class='excerpt'><?php the_excerpt(); ?></p>
 					</div>
@@ -64,7 +48,13 @@ $src = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), array( 560
 			wp_reset_postdata();?>
 		</div>
 
+		<div class='article-excerpt active'>
+			<p class='meta'>June 10, 2016</p>
+			<a href='#'>The Title and Permalink</a>
+			<p class='excerpt'>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+		</div>
 	</div>
+
 	<div id='unique-sidebar'>
 		<div class='half'>
 			<img src='http://images.clipartpanda.com/coffee-cup-clip-art-black-white-MTLqzMdTa.jpeg'>
@@ -81,63 +71,5 @@ $src = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), array( 560
 	</div>
 </section>
 
-<section id='middle'>
-	<div class='container'>
-
-	<!-- Category Specific Sections -->
-	<div class='category-wrap' id='training'>
-		<h3>Training</h3>
-		<div class='group'>
-
-			<!-- First Section: Show the most recent post in a category and also show the thumbnail -->
-			<?php
-			// Grab the first thre ...
-			$args = array( 'posts_per_page' => 1, 'offset'=> 0, 'category_name' => "training");
-			$myposts = get_posts( $args );
-			foreach ( $myposts as $post ) : setup_postdata( $post ); ?>
-
-				<div class='article-group'>
-					<!-- Set the feature_image as the background image -->
-					<?php $src = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), array( 5600,1000 ), false, '' ); ?>
-					<div class='preview-image' style="background-image: url(<?php echo $src[0]; ?> ) !important;"></div>
-
-					<!-- Fill the content box -->
-					<div class='preview-excerpt'>
-						<p class='meta'><?php the_time('F j, Y'); ?></p>
-						<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-						<p class='excerpt'>A shorter excerpt that is only about 5 lines long or so. It's difficult to d in Wordpress because Wordpress sucks so much, but rest assured - I'll have it. Mark these fine printed words, I'll have it</p>
-					</div>
-				</div>
-
-			<?php endforeach;
-			wp_reset_postdata();?>
-
-			<?php
-			// Grab the first thre ...
-			$args = array( 'posts_per_page' => 2, 'offset'=> 1, 'category_name' => "training");
-			$myposts = get_posts( $args );
-			foreach ( $myposts as $post ) : setup_postdata( $post ); ?>
-
-				<div class='article-group'>
-
-					<!-- Fill the content box -->
-					<div class='preview-excerpt'>
-						<p class='meta'><?php the_time('F j, Y'); ?></p>
-						<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-					</div>
-				</div>
-
-			<?php endforeach;
-			wp_reset_postdata();?>
-
-		</div>
-
-	</div>
-
-
-
-
-	</div>
-</section>
 
 <!-- <?php the_posts_navigation(); ?> -->
