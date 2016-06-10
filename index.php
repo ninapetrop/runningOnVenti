8bcae6e654797be71d9bcc7c0530f75e9bf5e171
Nina Petropoulos <petropoulos.nina@gmail.com>
2016-06-09 21:21:59 -0400

    home styles

--- index.php
+++ index.php
@@ -89,24 +89,41 @@ $src = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), array( 560
 		<h3>Training</h3>
 		<div class='group'>
 
+			<!-- First Section: Show the most recent post in a category and also show the thumbnail -->
 			<?php
 			// Grab the first thre ...
-			$args = array( 'posts_per_page' => 3, 'offset'=> 0, 'category_name' => "training");
+			$args = array( 'posts_per_page' => 1, 'offset'=> 0, 'category_name' => "training");
+			$myposts = get_posts( $args );
+			foreach ( $myposts as $post ) : setup_postdata( $post ); ?>
+
+				<div class='article-group'>
+					<!-- Set the feature_image as the background image -->
+					<?php $src = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), array( 5600,1000 ), false, '' ); ?>
+					<div class='preview-image' style="background-image: url(<?php echo $src[0]; ?> ) !important;"></div>
+
+					<!-- Fill the content box -->
+					<div class='preview-excerpt'>
+						<p class='meta'><?php the_time('F j, Y'); ?></p>
+						<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+						<p class='excerpt'>A shorter excerpt that is only about 5 lines long or so. It's difficult to d in Wordpress because Wordpress sucks so much, but rest assured - I'll have it. Mark these fine printed words, I'll have it</p>
+					</div>
+				</div>
+
+			<?php endforeach;
+			wp_reset_postdata();?>
+
+			<?php
+			// Grab the first thre ...
+			$args = array( 'posts_per_page' => 2, 'offset'=> 1, 'category_name' => "training");
 			$myposts = get_posts( $args );
 			foreach ( $myposts as $post ) : setup_postdata( $post ); ?>
 
 				<div class='article-group'>
 
-					<!-- Set the feature_image as the background image -->
-					<?php
-	$src = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), array( 5600,1000 ), false, '' );
-	?>
-					<div class='preview-image' style="background-image: url(<?php echo $src[0]; ?> ) !important;"></div>
-
 					<!-- Fill the content box -->
 					<div class='preview-excerpt'>
+						<p class='meta'><?php the_time('F j, Y'); ?></p>
 						<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-						<p class='excerpt'>A shorter excerpt that is only about 5 lines long or so. It's difficult to d in Wordpress because Wordpress sucks so much, but rest assured - I'll have it. Mark these fine printed words, I'll have it</p>
 					</div>
 				</div>
 
@@ -117,101 +134,7 @@ $src = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), array( 560
 
 	</div>
 
-	<div class='category-wrap' id='training'>
-		<h3>Training</h3>
-		<div class='group'>
 
-			<?php
-			// Grab the first thre ...
-			$args = array( 'posts_per_page' => 3, 'offset'=> 0, 'category_name' => "training");
-			$myposts = get_posts( $args );
-			foreach ( $myposts as $post ) : setup_postdata( $post ); ?>
-
-				<div class='article-group'>
-
-					<!-- Set the feature_image as the background image -->
-					<?php
-	$src = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), array( 5600,1000 ), false, '' );
-	?>
-					<div class='preview-image' style="background-image: url(<?php echo $src[0]; ?> ) !important;"></div>
-
-					<!-- Fill the content box -->
-					<div class='preview-excerpt'>
-						<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-						<p class='excerpt'>A shorter excerpt that is only about 5 lines long or so. It's difficult to d in Wordpress because Wordpress sucks so much, but rest assured - I'll have it. Mark these fine printed words, I'll have it</p>
-					</div>
-				</div>
-
-			<?php endforeach;
-			wp_reset_postdata();?>
-
-		</div>
-
-	</div>
-
-	<div class='category-wrap' id='training'>
-		<h3>Training</h3>
-		<div class='group'>
-
-			<?php
-			// Grab the first thre ...
-			$args = array( 'posts_per_page' => 3, 'offset'=> 0, 'category_name' => "training");
-			$myposts = get_posts( $args );
-			foreach ( $myposts as $post ) : setup_postdata( $post ); ?>
-
-				<div class='article-group'>
-
-					<!-- Set the feature_image as the background image -->
-					<?php
-	$src = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), array( 5600,1000 ), false, '' );
-	?>
-					<div class='preview-image' style="background-image: url(<?php echo $src[0]; ?> ) !important;"></div>
-
-					<!-- Fill the content box -->
-					<div class='preview-excerpt'>
-						<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-						<p class='excerpt'>A shorter excerpt that is only about 5 lines long or so. It's difficult to d in Wordpress because Wordpress sucks so much, but rest assured - I'll have it. Mark these fine printed words, I'll have it</p>
-					</div>
-				</div>
-
-			<?php endforeach;
-			wp_reset_postdata();?>
-
-		</div>
-
-	</div>
-
-	<div class='category-wrap' id='training'>
-		<h3>Training</h3>
-		<div class='group'>
-
-			<?php
-			// Grab the first thre ...
-			$args = array( 'posts_per_page' => 3, 'offset'=> 0, 'category_name' => "training");
-			$myposts = get_posts( $args );
-			foreach ( $myposts as $post ) : setup_postdata( $post ); ?>
-
-				<div class='article-group'>
-
-					<!-- Set the feature_image as the background image -->
-					<?php
-	$src = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), array( 5600,1000 ), false, '' );
-	?>
-					<div class='preview-image' style="background-image: url(<?php echo $src[0]; ?> ) !important;"></div>
-
-					<!-- Fill the content box -->
-					<div class='preview-excerpt'>
-						<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-						<p class='excerpt'>A shorter excerpt that is only about 5 lines long or so. It's difficult to d in Wordpress because Wordpress sucks so much, but rest assured - I'll have it. Mark these fine printed words, I'll have it</p>
-					</div>
-				</div>
-
-			<?php endforeach;
-			wp_reset_postdata();?>
-
-		</div>
-
-	</div>
 
 
 	</div>
